15484c7108156e56d08d8c42fbc716606828603a
Linet Ndau <mkandoelinet@gmail.com>
2024-04-03 09:06:51 +0300

    correction

--- array.js
+++ array.js
@@ -1,43 +1,45 @@
 
-function sorting (){
-    const state = ['Hello','World','Nancy','Daisy'];
-    console.log(state.sort());
-    console.log(state.reverse());
+function reverseStringInArray(arr){
+  const reversedStrings = arr.map(str =>[...str].reverse().join(''));
+  console.log(reversedStrings);
 }
-sorting()
+reverseStringInArray(["Hello","world","Nancy","Daisy"])
 
 
-function checkElements() {
-    let array =array.forEach(number => {
-       if (number > 0) {
-         console.log("positive");
-       } else if (number < 0) {
-         console.log("negative");
-       } else {
-         console.log("zero");
-       }
-    });
+
+function checkElement(array,index){
+  const number = array[index];
+
+  if(number > 0){
+    return number + "is positive";
+  }else if(number <0){
+    return number + "is negative";
+  }else{
+    return number + "is Zero"
+  }
 }
+const numbers =[1,3,6,8,9,10]
+console.log(checkElement(numbers,3))
 
-let employee = [
-    { id: 1 },
-    { name: "Jack" },
-    { salary: [2500, 3500, 4500, 15000] },
-  ];
-    employee.sort(function(c, d) {
-    return c.salary - (d.salary);
-  });
-  employee.forEach((person) => console.log(employee.salary));
- console.log(employee);
-
+function sortBySalary(employees){
+  return employees.sort((a,b)=> a.salary -b.salary)
+}
+const employees =[
+  {id:1,name:'Jane',salary:60000},
+  {id:2,name:'Josh',salary: 10000},
+  {id:3,name:'Nancy',salary:30000}
+];
+const sortedEmployees = sortBySalary(employees)
+console.log(sortedEmployees);
 
  
- function multiplication(numbers) {
+ 
+function multiplication(numbers) {
     numbers.forEach((item) => {
        console.log(item * 2);
     });
    }
-   multiply([5, 6, 7, 8]);
+   multiplication([5, 6, 7, 8]);
 
     
    
